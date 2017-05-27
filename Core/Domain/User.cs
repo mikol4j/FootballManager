59b1788563791085c7c20c93372efbd9c61cf2df
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-05-27 13:00:07 +0200

    Add Core,Infrastrucuture,TestsEndToEnd projects and User Model

---
+++ Core/Domain/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; protected set; }
+
+        public string Password { get; protected set; }
+
+        public string Salt { get; protected set; }
+
+        public string UserName { get; protected set; }
+
+        public string FullName { get; protected set; }
+
+        public DateTime CreatedAt { get; protected set; }
+
+        protected User()
+        {
+            //protect from creating parameterless instance
+        }
+
+        public User(string email, string username, string password, string salt )
+        {
+            Id = new Guid();
+            Email = email;
+            UserName = username;
+            Password = password;
+            Salt = salt;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+
+
+}
---
+++ TestsEndToEnd/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace TestsEndToEnd
+{
+    public class Class1
+    {
+    }
+}
