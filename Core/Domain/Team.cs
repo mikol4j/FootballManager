10f1c15e751db444445efa8c2945875348012c1f
Jankowski Mikołaj <mjankows@pentacomp.pl>
2017-05-30 14:28:47 +0200

    Add Team model

---
+++ Core/Domain/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+
+        public string TeamName { get; protected set; }
+
+        public string Description { get; protected set; }
+
+        public DateTime CreatedAt { get; protected set; }
+
+        protected Team()
+        {
+            //protects from creating parameterless instance
+        }
+
+        public Team(string teamName, string description)
+        {
+            Id = new Guid();
+            TeamName = teamName;
+            Description = description;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+
+
+}
