841ea297dd9be3e3545819752b476aedb45d7d46
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-06-04 17:51:31 +0200

    Add FluentAssertion and Moq to Tests project

---
+++ Tests/Services/UserServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Services
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task Test()
+        {
+            true.ShouldBeEquivalentTo(true);
+        }
+    }
+}
