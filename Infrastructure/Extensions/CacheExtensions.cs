46a1c67ca7299379160cdfb0eb7a387cc8090f67
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-07-25 20:06:56 +0200

    Add Cache Extension

---
+++ Infrastructure/Extensions/CacheExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Dto;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Extensions
+{
+    public static class CacheExtensions
+    {
+        public static void SetJwt(this IMemoryCache cacheMemory, Guid tokenId, JwtDto jwt)
+            => cacheMemory.Set(GetJwtKey(tokenId), jwt, TimeSpan.FromSeconds(5));
+
+        public static void GetJwt(this IMemoryCache cacheMemory, Guid tokenId)
+            => cacheMemory.Get<JwtDto>(GetJwtKey(tokenId));
+
+        public static string GetJwtKey(Guid tokenId)
+            => $"jwt-{tokenId}";
+    }
+}
