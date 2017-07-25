f26d8290b72665bd0995e4cebfff568decc22e86
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-07-25 19:57:39 +0200

    Add Microsoft.Extensions.Caching.Memory

---
+++ Infrastructure/Handlers/Users/LoginHandler.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Commands;
+using Infrastructure.Commands.Users;
+using Infrastructure.Services;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Handlers.Users
+{
+    class LoginHandler : ICommandHandler<Login>
+    {
+        private readonly IUserService _userService;
+        private readonly IJwtHandler _jwtHandler;
+        private readonly IMemoryCache _memoryCache;
+        public LoginHandler(IUserService userService, IJwtHandler jwtHandler, IMemoryCache memoryCache)
+        {
+            _userService = userService;
+            _jwtHandler = jwtHandler;
+            _memoryCache = memoryCache;
+        }
+        public async Task HadnleAsync(Login command)
+        {
+            await _userService.LoginAsync(command.Email, command.Password);
+        }
+    }
+}
