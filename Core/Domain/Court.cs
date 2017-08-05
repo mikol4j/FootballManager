4da1924d0cb800fdeb76d4d0add1955f64bcb198
Mikołaj Jankowski <mikolaj.jankowski1@gmail.com>
2017-08-05 22:05:45 +0200

    Add Court entity, ICourtProvider

---
+++ Core/Domain/Court.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain
+{
+    public class Court
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
